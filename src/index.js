f4a54d1e487094b7977c9eca4e447b7930ee56c8
Laith Massoud <laythmassoud8@gmail.com>
2017-11-29 13:34:08 +0100

    new branch

--- src/index.js
+++ src/index.js
@@ -1,120 +1,50 @@
 import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
-import navbar from './tamplates/navbar.html';
+import navbartemplate from './templates/navbar.html';
+import carouselTemplate from './templates/carousel.html';
+import carouselItemTemplate from './templates/carousel-item.html';
 
-const pictures = [
-  'lion.jpg',
-  'lion3.jpg',
-  'lion4.jpg',
-  'lion5.jpg',
-  'lion6.jpg',
-];
 
-const carousel = `
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item container active">
-      <div class="row">
-        <div class="col-md-6 ">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 ">
-          <img class="d-block w-100" src="static/lion.jpg" alt="First slide" style="width:100%; height: 300px;">
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item container ">
-      <div class="row">
-        <div class="col-md-6 ">
-          <img class="d-block w-100" src="static/lion5.jpg" alt="First slide" style="width:100%; height: 300px;">
-        </div>
-        <div class="col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item container ">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 ">
-          <img class="d-block w-100" src="static/lion4.jpg" alt="First slide" style="width:100%; height: 300px;">
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item container ">
-      <div class="row">
-        <div class="col-md-6 ">
-          <img class="d-block w-100" src="static/lion5.jpg" alt="First slide" style="width:100%; height: 300px;">
-        </div>
-        <div class="col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-`;
-
-function mkCard(img) {
-  return `
-<div class="card">
-  <img class="card-img-top" src="static/${img}" alt="Card image cap">
-  <div class="card-body">
-    <h4 class="card-title">Card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-`;
+function mkIndicator(number) {
+  return $(`<li data-target="#carousel-indicators" data-slide-to="${number}" class="active"></li>`);
 }
 
-const grid = `
-<div class="container">
-  <div class ="row">
-  <div class="col-12 col-lg-4" > ${mkCard(pictures[0])}</div>
-  <div class="col-12 col-lg-4" > ${mkCard(pictures[2])}</div>
-  <div class="col-12 col-lg-4" > ${mkCard(pictures[3])}</div>
-  </div>
-</div>
-`;
+function mkSlide(item) {
+  const $el = $(carouselItemTemplate);
+  $el.find('h2').text(item.name);
+  return $el;
+}
+
+function mkCarousel(items) {
+  const $el = $(carouselTemplate);
+  const $indicators = $el.find('.carousel-indicators');
+  const $slides = $el.find('.carousel-inner');
+  items.forEach((item, number) => {
+    const $indicator = mkIndicator(number);
+    const $slide = mkSlide(item);
+
+    if (number === 0) {
+      $slide.addClass('active');
+      $indicator.addClass('active');
+    }
+
+    $indicators.append($indicator);
+    $slides.append($slide);
+  });
+
+  return $el;
+}
+const catogories = [
+  { name: 'first' },
+  { name: 'second' },
+];
 
 
 $(() => {
+  const $carousel = mkCarousel(catogories);
   $('#root')
-    .append(navbar)
-    .append(carousel)
-    .append(grid);
+    .append(navbartemplate)
+    .append($carousel);
+  $carousel.carousel();
 });
