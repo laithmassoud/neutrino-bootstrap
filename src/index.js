31dafc1bc796add97f2e978d780a48d249a222a6
Laith Massoud <laythmassoud8@gmail.com>
2017-12-13 11:06:18 +0100

    new tools

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
 import navbarTemplate from './templates/navbar.html';
-//  TODO 2.2: Add a new template for the modal windows
-import ModalTemplate from './templates/modal.html';
-
+import modalTemplate from './templates/modal.html';
 import mkCarousel from './carousel';
-//  TODO 1.3: Add the new functions from products to import
-import { mkProductCard, refreshProdcts } from './products';
-
+import refreshProducts from './products';
 
 //  append navbar
 $(() => {
-  //  TODO 2.3: Append modal windows tpl
-  $('#root').append(ModalTemplate);
-
-  $('#root').append(navbarTemplate);
+  $('#root').append(modalTemplate)
+    .append(navbarTemplate);
   //  read categories
   $.ajax('./static/categories.json')
     .done((categories) => {
@@ -27,52 +21,54 @@ $(() => {
 
       //  Iterate over the categories and append to navbar
       categories.forEach((category, number) => {
-        //  TODO 1.1: Add data attributes to the links: data-name
         $('.navbar-nav').append(`
             <li class="nav-item">
             <a class="nav-link" data-id="${number}" data-name="${category.name}" href="#">${category.name}</a>
             </li>`);
       });
+      $('.navbar-nav').append('<span class="glyphicon glyphicon-shopping-cart"></span>');
     })
     //  or fail trying
     .fail((xhr, status, error) => {
-      $('#root').append(`<div> Ajax Error categories: ${error} </div>`);
+      $('#root').append(`<div>Ajax Error categories: ${error}</div>`);
     });
 
   //  ajax req and append products grid
   $.ajax('./static/products.json')
     .done((products) => {
-      //  TODO 1.5: Add Counter
-      $('#root').append(`<div class="infobox"><h2 id="infos">All products(${Object.keys(products).length})</h2></div>`);
-
       //  append products-grid after carousel
+      $('#root').append(`<div class="infobox"><h2 id="infos">All products (${Object.keys(products).length})</h2></div>`);
       $('#root').append('<div id="products-grid" class="container-fluid"></div>');
       //  populate products-grid with products
       $('#products-grid').append('<div class="row"></div>');
-      products.forEach((product) => {
-        $('.row').append(mkProductCard(product));
-      });
-      // TODO 1.2: click event handler for nav-links
-      $('.nav-link').click((event) => {
-        const { target } = event;
-        const linkname = target.getAttribute('data-name');
-        $(target).closest('ul').find('.active').removeClass('active');
+      refreshProducts(products, '-1');
+      // click event handler on nav-links
+      $('.nav-link').click((eventObj) => {
+        const { target } = eventObj;
+        const linkName = target.getAttribute('data-id');
+        $('.navbar-nav .active').removeClass('active');
         $(target).closest('li').addClass('active');
-        $('#products-grid').empty();
-        refreshProdcts(products, linkname);
+        //  clean the products-grid and update the content
+        refreshProducts(products, linkName);
       });
-
-      // TODO 2.6: click event handler for details button
-
-      $('.detailsButton').click((event) => {
-        const { target } = event;
-        const productImg = target.getAttribute('data-img');
-        $('.modal-title').text(`More info about ${target.getAttribute('data-name')}`);
-        $('.modal-body').html(`the price is ${target.getAttribute('data-price')} € <br> <img  style="width:400px;height:300px;" src="${productImg}"> <br> <p> A laptop, often called a notebook or "notebook computer", is a small, portable personal computer with a "clamshell" form factor, an alphanumeric keyboard on the lower part of the "clamshell" and a thin LCD or LED computer screen on the upper part, which is opened up to use the computer. Laptops are folded shut for transportation, and thus are suitable for mobile use.[1] Although originally there was a distinction between laptops and notebooks, the former being bigger and heavier than the latter, as of 2014, there is often no longer any difference.[2] Laptops are commonly used in a variety of settings, such as at work, in education, in playing games, Internet surfing, for personal multimedia and general home computer use.
-        </p>`);
-        $('#detailsModal').modal('toggle');
-      });
+    })
+    //  or fail trying
+    .fail((xhr, status, error) => {
+      $('#root').append(`<div>Ajax Error products: ${error}</div>`);
     });
-  //  or fail trying TODO: BONUS
+  $('.addCart').click(() => {
+    //  localStorage.clear();
+
+    if (localStorage.getItem('total') === null) {
+      console.log('empty');
+      const total = 1;
+      localStorage.setItem('total', total);
+    } else {
+      const total = parseInt(localStorage.getItem('total'), 10);
+      localStorage.setItem('total', total + 1);
+    }
+    console.log('Add to cart');
+    console.log(localStorage.getItem('total'));
+  });
   // End
 });
--- src/products.js
+++ src/products.js
@@ -7,36 +7,39 @@ export function mkProductCard(product) {
   $el.find('div:nth-child(1)').addClass(`card ${product.category_id}`);
   $el.find('.card-title').text(product.name);
   $el.find('.card-text').text(`Price: ${product.price}€`);
-  $el.find('.card-img-top').attr('src', `./static/assets/images/${product.type}.jpg`);
+  $el.find('.card-img-top').attr('src', `./static/assets/images/0${product.category_id}.jpg`);
   $el.find('.detailsButton').attr('data-name', `${product.name}`);
+  $el.find('.detailsButton').attr('data-id', `${product.category_id}`);
   $el.find('.detailsButton').attr('data-price', `${product.price}`);
-  $el.find('.detailsButton').attr('data-img', `./static/assets/images/${product.type}.jpg`);
   return $el;
 }
-
 //  filter and refresh the products
-//  TODO 1.3: Add refresh Products
-export function refreshProdcts(products, type) {
+export default function refreshProducts(products, type) {
+  $('#products-grid').empty();
   $('#products-grid').append('<div class="row"></div>');
-  if (type === 'All') {
+  const cat = parseInt(type, 10);
+  //  check if request all product
+  if (cat === -1) {
     products.forEach((product) => {
       $('.row').append(mkProductCard(product));
     });
     $('#infos').text(`All products (${Object.keys(products).length})`);
   } else {
-    products.filter(product => product.type === type).forEach((product) => {
-      $('.row').append(mkProductCard(product));
-    });
-    const activcategory = $('.navbar-nav .active').text();
-
-    $('#infos').text(`Total products in ${activcategory} (${Object.keys(products.filter(product => product.type === type)).length})`);
+    //  request only one product
+    products.filter(product => product.category_id === cat)
+      .forEach((product) => {
+        $('.row').append(mkProductCard(product));
+      });
+    const activeCategory = $('.navbar-nav .active').text();
+    console.log(activeCategory);
+    $('#infos').text(`Total products in ${activeCategory} (${Object.keys(products.filter(product => product.category_id === cat)).length})`);
   }
-  $('.detailsButton').click((event) => {
-    const { target } = event;
-    const productImg = target.getAttribute('data-img');
+  $('.detailsButton').click((eventObj) => {
+    const { target } = eventObj;
     $('.modal-title').text(`More info about ${target.getAttribute('data-name')}`);
-    $('.modal-body').html(`the price is ${target.getAttribute('data-price')} € <br> <img  style="width:400px;height:300px;" src="${productImg}"> <br> <p> A laptop, often called a notebook or "notebook computer", is a small, portable personal computer with a "clamshell" form factor, an alphanumeric keyboard on the lower part of the "clamshell" and a thin LCD or LED computer screen on the upper part, which is opened up to use the computer. Laptops are folded shut for transportation, and thus are suitable for mobile use.[1] Although originally there was a distinction between laptops and notebooks, the former being bigger and heavier than the latter, as of 2014, there is often no longer any difference.[2] Laptops are commonly used in a variety of settings, such as at work, in education, in playing games, Internet surfing, for personal multimedia and general home computer use.
-    </p>`);
+    $('.modal-image').attr('src', `./static/assets/images/0${target.getAttribute('data-id')}.jpg`);
+    $('.modal-body').text(`The price of this product is € ${target.getAttribute('data-price')}`);
+    $('.modal-total').text(`Total 1x ${target.getAttribute('data-name')} is € ${target.getAttribute('data-price')}`);
     $('#detailsModal').modal('toggle');
   });
 }
